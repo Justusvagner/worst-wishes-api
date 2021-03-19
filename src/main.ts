eb02b8ff0e2b13f43bd347b15db083de318a01f3
eugenek <eugene@paydock.com>
2021-03-20 00:46:41 +0200

    init

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { Logger } from "@nestjs/common";
+
+import { AppModule } from './app.module';
+import 'dotenv/config';
+
+const port = process.env.PORT || 8080;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(port);
+  Logger.log(`Server is running on http://localhost:${port}`, 'Bootstrap')
+}
+bootstrap();
