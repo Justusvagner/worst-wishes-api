19710d164858b188aa730cfae87228724085f6d1
eugenek <eugene@paydock.com>
2021-03-20 11:21:35 +0200

    cors

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,10 @@ const port = process.env.PORT || 8080;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    methods: ['GET', 'POST', 'PUT'],
+    origin: ['31.202.47.15:8081', '31.202.47.15:8080'],
+  })
   await app.listen(port);
   Logger.log(`Server is running on http://localhost:${port}`, 'Bootstrap')
 }
