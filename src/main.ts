3f5beb3cd1e1ffd2db50f1114167e0fd29434d46
eugenek <eugene@paydock.com>
2021-03-20 11:31:24 +0200

    env

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,13 @@ import { AppModule } from './app.module';
 import 'dotenv/config';
 
 const port = process.env.PORT || 8080;
+const origin = process.env.ORIGIN || '*'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     methods: ['GET', 'POST', 'PUT'],
-    origin: ['31.202.47.15:8081', '31.202.47.15:8080'],
+    origin: origin,
   })
   await app.listen(port);
   Logger.log(`Server is running on http://localhost:${port}`, 'Bootstrap')
